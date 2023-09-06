3b9e557b1aa6725b2d08d8fd37f27eb2d32672b9
MDeMel-VU <Manendra.demel@reece.com.au>
2023-09-06 18:41:18 +1000

    Create new intent and navigate to second screen

--- app/src/main/java/com/mdemel/afl/mainScreen/MainActivity.kt
+++ app/src/main/java/com/mdemel/afl/mainScreen/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.mdemel.afl.mainScreen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.mdemel.afl.R
+import com.mdemel.afl.secondScreen.SecondActivity
 
 class MainActivity : AppCompatActivity() {
+
+    val intentToSecondScreen = Intent(this, SecondActivity::class.java)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonToNavigateToSecondScreen = findViewById<Button>(R.id.navigateButton)
+
+        buttonToNavigateToSecondScreen.setOnClickListener {
+
+            startActivity(intentToSecondScreen)
+        }
     }
 }
