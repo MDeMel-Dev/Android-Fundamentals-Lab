fd7d0999cd4fbb42f82af6c50ce27cfab31ba8d6
MDeMel-VU <Manendra.demel@reece.com.au>
2023-09-07 21:52:15 +1000

    Squashed commit of the following:
    commit 6a6a7f73a580181f3353fa0daa28eecbfcb41cc9
    Author: MDeMel-VU <Manendra.demel@reece.com.au>
    Date:   Thu Sep 7 21:51:04 2023 +1000
    
        Setup callback to collect text being entered
    
    commit cda185e070b37d06314a0511d3ac1b23421ba919
    Author: MDeMel-VU <Manendra.demel@reece.com.au>
    Date:   Thu Sep 7 21:40:39 2023 +1000
    
        Add a radius to the corner of editText
    
    commit 2e76447ff652a90f9b7ad49f9387ed15eb228e56
    Author: MDeMel-VU <Manendra.demel@reece.com.au>
    Date:   Thu Sep 7 21:40:17 2023 +1000
    
        Add EditText - collect input information component
    
    commit a683e7ab5d1a86cb9b24d2ad136c6d5bc68bf2a7
    Author: MDeMel-VU <Manendra.demel@reece.com.au>
    Date:   Thu Sep 7 21:31:26 2023 +1000
    
        Add a toolbar to App Theme

--- app/src/main/java/com/mdemel/afl/HomeScreen/MainActivity.kt
+++ app/src/main/java/com/mdemel/afl/HomeScreen/MainActivity.kt
@@ -4,20 +4,33 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import com.mdemel.afl.R
 import com.mdemel.afl.SecondScreen.InfoActivity
 
 class MainActivity : AppCompatActivity() {
     lateinit private var navigateButton: Button
+    lateinit private var textCollectionForm: EditText
+
+    var textHolder = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val intentToNavigateToInfoScreen = Intent(this, InfoActivity::class.java)
 
+        // Get references to the views in our layout file
         navigateButton = findViewById(R.id.navigateButtonToInfo)
+        textCollectionForm = findViewById(R.id.textCollectForm)
 
+        // set click listners/callbacks
         navigateButton.setOnClickListener {
             startActivity(intentToNavigateToInfoScreen)
+        }
+
+        textCollectionForm.addTextChangedListener {
+            textHolder = it.toString()
         }
     }
 }
