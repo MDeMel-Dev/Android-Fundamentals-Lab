d44c7567e5bc9c22a042196bea5f198219469403
MDeMel-VU <Manendra.demel@reece.com.au>
2023-09-07 20:51:43 +1000

    Create intent and startActivity on button Click

--- app/src/main/java/com/mdemel/afl/HomeScreen/MainActivity.kt
+++ app/src/main/java/com/mdemel/afl/HomeScreen/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.mdemel.afl.HomeScreen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.mdemel.afl.R
+import com.mdemel.afl.SecondScreen.InfoActivity
 
 class MainActivity : AppCompatActivity() {
     lateinit private var navigateButton: Button
+
+    val intentToNavigateToInfoScreen = Intent(this, InfoActivity::class.java)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         navigateButton = findViewById(R.id.navigateButtonToInfo)
+
+        navigateButton.setOnClickListener {
+            startActivity(intentToNavigateToInfoScreen)
+        }
     }
 }
