ce0fd576592ab41dc1e15bc244d6e62aa071cce9
MDeMel-VU <Manendra.demel@reece.com.au>
2023-09-07 22:27:37 +1000

    Implement Adapter

---
+++ app/src/main/java/com/mdemel/afl/SecondScreen/recyclerviewComponents/InfoScreenRecyclerAdapter.kt
@@ -0,0 +1,27 @@
+package com.mdemel.afl.SecondScreen.recyclerviewComponents
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.mdemel.afl.R
+
+class InfoScreenRecyclerAdapter(val dataList: List<String>): RecyclerView.Adapter<InfoScreenListItemViewHolder>() {
+
+    override fun getItemCount(): Int {
+        return dataList.size
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): InfoScreenListItemViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.viewholder_layout, parent, false)
+
+        return InfoScreenListItemViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: InfoScreenListItemViewHolder, position: Int) {
+         holder.firsttext?.text = dataList[position]
+    }
+}
