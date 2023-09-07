5f3bc16251eb44f6b9477ec4ec049e528882282b
MDeMel-VU <Manendra.demel@reece.com.au>
2023-09-07 22:33:42 +1000

    set Adapter to Recyclerview in Activity

--- app/src/main/java/com/mdemel/afl/SecondScreen/InfoActivity.kt
+++ app/src/main/java/com/mdemel/afl/SecondScreen/InfoActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.RecyclerView
 import com.mdemel.afl.R
+import com.mdemel.afl.SecondScreen.recyclerviewComponents.InfoScreenRecyclerAdapter
 
 class InfoActivity : AppCompatActivity() {
 
@@ -13,5 +14,7 @@ class InfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
+         val dataList = listOf<String>("Donouts",  "Pizzas", "Rolls")
+         infoRecyclerview.adapter = InfoScreenRecyclerAdapter(dataList)
     }
 }
