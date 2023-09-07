cc00258f693376575e23fff3edc2d7e68af9c469
MDeMel-VU <Manendra.demel@reece.com.au>
2023-09-07 22:38:12 +1000

    Squashed commit of the following:
    commit 5f3bc16251eb44f6b9477ec4ec049e528882282b
    Author: MDeMel-VU <Manendra.demel@reece.com.au>
    Date:   Thu Sep 7 22:33:42 2023 +1000
    
        set Adapter to Recyclerview in Activity
    
    commit ce0fd576592ab41dc1e15bc244d6e62aa071cce9
    Author: MDeMel-VU <Manendra.demel@reece.com.au>
    Date:   Thu Sep 7 22:27:37 2023 +1000
    
        Implement Adapter
    
    commit 952a613b3a1d967beb5945ce5ab225791ca44814
    Author: MDeMel-VU <Manendra.demel@reece.com.au>
    Date:   Thu Sep 7 22:17:19 2023 +1000
    
        create viewholder layout and class
    
    commit be72ab619bd35c3859e064e95cf1e6085524253d
    Author: MDeMel-VU <Manendra.demel@reece.com.au>
    Date:   Thu Sep 7 22:03:26 2023 +1000
    
        Add recyclerview to layout and get reference in activity

--- app/src/main/java/com/mdemel/afl/SecondScreen/InfoActivity.kt
+++ app/src/main/java/com/mdemel/afl/SecondScreen/InfoActivity.kt
@@ -2,11 +2,19 @@ package com.mdemel.afl.SecondScreen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 import com.mdemel.afl.R
+import com.mdemel.afl.SecondScreen.recyclerviewComponents.InfoScreenRecyclerAdapter
 
 class InfoActivity : AppCompatActivity() {
+
+    val infoRecyclerview: RecyclerView by lazy {
+        findViewById(R.id.infoScreenRecyclerview)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
+         val dataList = listOf<String>("Donouts",  "Pizzas", "Rolls")
+         infoRecyclerview.adapter = InfoScreenRecyclerAdapter(dataList)
     }
 }
