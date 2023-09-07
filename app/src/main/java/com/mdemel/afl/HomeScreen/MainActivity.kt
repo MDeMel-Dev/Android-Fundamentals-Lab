e5b68f97ff2281719663f7b15c98cd4c44ac0149
MDeMel-VU <Manendra.demel@reece.com.au>
2023-09-07 20:42:31 +1000

    Create new activity
    Named the activity as InfoActivity

---
+++ app/src/main/java/com/mdemel/afl/HomeScreen/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mdemel.afl.HomeScreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mdemel.afl.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
