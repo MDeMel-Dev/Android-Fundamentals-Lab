be72ab619bd35c3859e064e95cf1e6085524253d
MDeMel-VU <Manendra.demel@reece.com.au>
2023-09-07 22:03:26 +1000

    Add recyclerview to layout and get reference in activity

--- app/src/main/java/com/mdemel/afl/SecondScreen/InfoActivity.kt
+++ app/src/main/java/com/mdemel/afl/SecondScreen/InfoActivity.kt
@@ -2,9 +2,14 @@ package com.mdemel.afl.SecondScreen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 import com.mdemel.afl.R
 
 class InfoActivity : AppCompatActivity() {
+
+    val infoRecyclerview: RecyclerView by lazy {
+        findViewById(R.id.infoScreenRecyclerview)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
