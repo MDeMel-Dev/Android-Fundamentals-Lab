63fb3c7f282221a2e2517a82c14dfd4bfc1717d1
MDeMel-VU <Manendra.demel@reece.com.au>
2023-09-07 20:49:31 +1000

    Get a reference to the button in Activity OnCreate

--- app/src/main/java/com/mdemel/afl/HomeScreen/MainActivity.kt
+++ app/src/main/java/com/mdemel/afl/HomeScreen/MainActivity.kt
@@ -2,11 +2,15 @@ package com.mdemel.afl.HomeScreen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.mdemel.afl.R
 
 class MainActivity : AppCompatActivity() {
+    lateinit private var navigateButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navigateButton = findViewById(R.id.navigateButtonToInfo)
     }
 }
