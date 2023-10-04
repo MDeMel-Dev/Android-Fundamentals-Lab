007348214ac3a8ca83200d1659b4b753066fc91f
MDeMel-Dev <mdemel.dev@gmail.com>
2023-10-04 21:44:52 +1100

    Implement basic shape app

--- app/src/main/java/com/mdemel/afl/MainActivity.kt
+++ app/src/main/java/com/mdemel/afl/MainActivity.kt
@@ -2,10 +2,36 @@ package com.mdemel.afl
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+    private var shapesImageView: ImageView? = null
+    private var changeShapeButton: Button? = null
+
+    private val listOfShapeDrawables =
+        listOf(R.drawable.img_cube, R.drawable.img_pyramid, R.drawable.img_sphere)
+
+    private var currentShape: Int? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        shapesImageView = findViewById(R.id.imageViewShapes) as? ImageView
+        changeShapeButton = findViewById(R.id.buttonChangeShape) as? Button
+
+        changeShapeButton?.setOnClickListener {
+            currentShape = swapShape()
+
+            currentShape?.let { shape ->
+                shapesImageView?.setImageResource(shape);
+            }
+        }
+    }
+
+    fun swapShape(): Int {
+        return listOfShapeDrawables.filterNot { it.equals(currentShape) }.random()
     }
 }
