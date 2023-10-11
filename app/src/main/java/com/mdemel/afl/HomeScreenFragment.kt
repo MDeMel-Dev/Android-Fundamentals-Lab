f0f3d872e8720d8c2c435b8b9efb1a825ebf99c3
MDeMel-Dev <mdemel.dev@gmail.com>
2023-10-11 21:15:20 +1100

    create a fragment

---
+++ app/src/main/java/com/mdemel/afl/HomeScreenFragment.kt
@@ -0,0 +1,30 @@
+package com.mdemel.afl
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class HomeScreenFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = HomeScreenFragment()
+    }
+
+    private val viewModel: HomeScreenViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_home_screen, container, false)
+    }
+}
