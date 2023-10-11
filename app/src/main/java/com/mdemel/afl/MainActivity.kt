f58267ea7089b78d4195569c6a7c9152dc07afe4
MDeMel-Dev <mdemel.dev@gmail.com>
2023-10-11 18:47:24 +1100

    Add edit text to collect data from user

--- app/src/main/java/com/mdemel/afl/MainActivity.kt
+++ app/src/main/java/com/mdemel/afl/MainActivity.kt
@@ -6,11 +6,17 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
+import androidx.core.widget.addTextChangedListener
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
 
     private var button: AppCompatButton? = null
+    private var inputTextField: TextInputEditText? = null
+
+    private var insertedText = ""
 
     private val intentToNavigateToSecondScreen by lazy {
         Intent(this, SecondScreenActivity::class.java)
@@ -21,11 +27,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         button = findViewById(R.id.buttonHomeScreen) as? AppCompatButton
+        inputTextField = findViewById(R.id.inputField) as? TextInputEditText
 
         button?.setOnClickListener {
             Log.d("MelbClass", "Button clicked ")
+            Toast.makeText(this, "User inserted: $insertedText", Toast.LENGTH_LONG).show()
+//            startActivity(intentToNavigateToSecondScreen)
 
-            startActivity(intentToNavigateToSecondScreen)
+        }
+
+        inputTextField?.addTextChangedListener { textValue ->
+            insertedText = textValue.toString()
         }
     }
 }
