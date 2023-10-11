7cea8f3496258947dedeb18a021fb9e33bf6cc83
MDeMel-Dev <mdemel.dev@gmail.com>
2023-10-11 21:38:08 +1100

    add first fragment to Activity

--- app/src/main/java/com/mdemel/afl/MainActivity.kt
+++ app/src/main/java/com/mdemel/afl/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
+import androidx.fragment.app.FragmentContainerView
+import androidx.fragment.app.commit
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +14,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.commit {
+            add(R.id.fragment_container_view, HomeScreenFragment::class.java, null)
+            setReorderingAllowed(true)
+            addToBackStack("name") // Name can be null
+        }
+
     }
 }
