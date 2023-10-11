429804fc9b82969e3b3df1c611ea058d073f5f20
MDeMel-Dev <mdemel.dev@gmail.com>
2023-10-11 20:37:43 +1100

    step 3.2 complete Implementing Adapter Class

--- app/src/main/java/com/mdemel/afl/recyclerviewComponents/ListItem.kt
+++ app/src/main/java/com/mdemel/afl/recyclerviewComponents/ListItem.kt
@@ -1,14 +1,14 @@
 package com.mdemel.afl.recyclerviewComponents
 
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.widget.AppCompatButton
-import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.mdemel.afl.R
 
-class ViewHolderType1(itemView: View) : ViewHolder(itemView) {
+class ListItem(itemView: View) : ViewHolder(itemView) {
 
-    var firstTile: CardView = itemView.findViewById(R.id.firstTile)
-    var secondTile: CardView = itemView.findViewById(R.id.secondTile)
+    var firstTileText: TextView = itemView.findViewById(R.id.firstTileText)
+    var secondTileText: TextView = itemView.findViewById(R.id.secondTileText)
     var button: AppCompatButton = itemView.findViewById(R.id.secondTile)
 }
--- app/src/main/java/com/mdemel/afl/recyclerviewComponents/MyRecyclerviewAdapter.kt
+++ app/src/main/java/com/mdemel/afl/recyclerviewComponents/MyRecyclerviewAdapter.kt
@@ -1,20 +1,30 @@
 package com.mdemel.afl.recyclerviewComponents
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView.Adapter
+import com.mdemel.afl.R
 import com.mdemel.afl.models.ListItem
 
-class MyRecyclerviewAdapter(val context: Context, val dataList: List<ListItem>): Adapter<ViewHolderType1>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderType1 {
-        TODO("Not yet implemented")
+class MyRecyclerviewAdapter(val context: Context, val dataList: List<ListItem>): Adapter<com.mdemel.afl.recyclerviewComponents.ListItem>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): com.mdemel.afl.recyclerviewComponents.ListItem {
+        // Create a new view, which defines the UI of the list item
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.layout_viewholder, parent, false)
+
+        return ListItem(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return dataList.size
     }
 
-    override fun onBindViewHolder(holder: ViewHolderType1, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(listItem: com.mdemel.afl.recyclerviewComponents.ListItem, position: Int) {
+        dataList.get(position).let { data ->
+            listItem.firstTileText.text = data.firstTile
+            listItem.secondTileText.text = data.secondTile
+            listItem.button.text = data.button
+        }
     }
 }
