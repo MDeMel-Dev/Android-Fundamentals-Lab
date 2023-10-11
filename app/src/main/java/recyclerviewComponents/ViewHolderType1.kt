8ed12be980f187168fe11cb1eda79e80e9fd580d
MDeMel-Dev <mdemel.dev@gmail.com>
2023-10-11 20:16:47 +1100

    step 2.2 - Implement ViewHolder class

---
+++ app/src/main/java/recyclerviewComponents/ViewHolderType1.kt
@@ -0,0 +1,14 @@
+package recyclerviewComponents
+
+import android.view.View
+import androidx.appcompat.widget.AppCompatButton
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.mdemel.afl.R
+
+class ViewHolderType1(itemView: View) : ViewHolder(itemView) {
+
+    var firstTile: CardView = itemView.findViewById(R.id.firstTile)
+    var secondTile: CardView = itemView.findViewById(R.id.secondTile)
+    var button: AppCompatButton = itemView.findViewById(R.id.secondTile)
+}
