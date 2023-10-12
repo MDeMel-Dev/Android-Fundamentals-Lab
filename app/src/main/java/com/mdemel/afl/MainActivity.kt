47c12a397d50a7730f04d9376ff24d2205f4dd8f
MDeMel-Dev <mdemel.dev@gmail.com>
2023-10-12 19:42:34 +1100

    Implement MutableStateflow to capture async work

--- app/src/main/java/com/mdemel/afl/MainActivity.kt
+++ app/src/main/java/com/mdemel/afl/MainActivity.kt
@@ -2,16 +2,29 @@ package com.mdemel.afl
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.ImageView
+import android.widget.Toast
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
-
+    private var newStringDataFlow = MutableStateFlow("Initial value")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        CoroutineScope(Dispatchers.IO).launch {
+            delay(10000) // Think of this as some work that would create a delay or block in your execution
+            newStringDataFlow.value = "New Value"
+        }
+
+        CoroutineScope(Dispatchers.Main).launch{
+            newStringDataFlow.collect {
+                Toast.makeText(this@MainActivity, it, Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
