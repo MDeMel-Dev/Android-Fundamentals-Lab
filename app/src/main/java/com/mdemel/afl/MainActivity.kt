8febe99bd5c92dba75d89d851e0816100958b574
MDeMel-Dev <mdemel.dev@gmail.com>
2023-10-12 20:50:42 +1100

    Implement network call function execution

--- app/src/main/java/com/mdemel/afl/MainActivity.kt
+++ app/src/main/java/com/mdemel/afl/MainActivity.kt
@@ -5,10 +5,23 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
+import androidx.lifecycle.MutableLiveData
+import com.mdemel.afl.api.CatFactApi
+import com.mdemel.afl.model.CatFactResponse
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private val textLabel: TextView by lazy {
+        findViewById(R.id.textView)
+    }
 
     private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
@@ -17,9 +30,32 @@ class MainActivity : AppCompatActivity() {
             .build()
     }
 
+    private val responseLiveData = MutableLiveData<CatFactResponse?>(null)
+
+    private val button: AppCompatButton by lazy {
+        findViewById(R.id.buttonGetFact)
+    }
+
+    private val catFactApi: CatFactApi by lazy {
+        retrofit.create(CatFactApi::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        responseLiveData.observe(this) { response ->
+            textLabel.text = response?.fact ?: "Hello World\nWelcome to the world of\nCat Facts"
+        }
+
+        button.setOnClickListener {
+            executeGetCatFactCall()
+        }
+    }
+
+    fun executeGetCatFactCall() {
+        CoroutineScope(Dispatchers.Main).launch {
+            responseLiveData.value = catFactApi.getCatFact()
+        }
     }
 }
