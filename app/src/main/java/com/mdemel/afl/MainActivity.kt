02144cb09ed4d53fc41062cc3d54255b8d9e20da
MDeMel-Dev <mdemel.dev@gmail.com>
2023-10-12 21:13:10 +1100

    restructure the code into viewmodel

--- app/src/main/java/com/mdemel/afl/MainActivity.kt
+++ app/src/main/java/com/mdemel/afl/MainActivity.kt
@@ -21,45 +21,28 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
-    private val viewModel = ViewModelProvider(this).get(MainScreenViewModel::class.java)
+    private val viewModel by lazy {
+        ViewModelProvider(this).get(MainScreenViewModel::class.java)
+    }
 
     private val textLabel: TextView by lazy {
         findViewById(R.id.textView)
     }
 
-    private val retrofit: Retrofit by lazy {
-        Retrofit.Builder()
-            .baseUrl("https://catfact.ninja") // Add the correct base url
-            .addConverterFactory(MoshiConverterFactory.create())
-            .build()
-    }
-
-    private val responseLiveData = MutableLiveData<CatFactResponse?>(null)
-
     private val button: AppCompatButton by lazy {
         findViewById(R.id.buttonGetFact)
-    }
-
-    private val catFactApi: CatFactApi by lazy {
-        retrofit.create(CatFactApi::class.java)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        responseLiveData.observe(this) { response ->
+        viewModel.responseLiveData.observe(this) { response ->
             textLabel.text = response?.fact ?: "Hello World\nWelcome to the world of\nCat Facts"
         }
 
         button.setOnClickListener {
-            executeGetCatFactCall()
-        }
-    }
-
-    fun executeGetCatFactCall() {
-        CoroutineScope(Dispatchers.Main).launch {
-            responseLiveData.value = catFactApi.getCatFact()
+            viewModel.executeGetCatFactCall()
         }
     }
 }
--- app/src/main/java/com/mdemel/afl/MainScreenViewModel.kt
+++ app/src/main/java/com/mdemel/afl/MainScreenViewModel.kt
@@ -1,7 +1,33 @@
 package com.mdemel.afl
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.mdemel.afl.api.CatFactApi
+import com.mdemel.afl.model.CatFactResponse
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 class MainScreenViewModel: ViewModel() {
 
+    val responseLiveData = MutableLiveData<CatFactResponse?>(null)
+
+    val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl("https://catfact.ninja") // Add the correct base url
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+    }
+
+    val catFactApi: CatFactApi by lazy {
+        retrofit.create(CatFactApi::class.java)
+    }
+
+    fun executeGetCatFactCall() {
+        CoroutineScope(Dispatchers.Main).launch {
+            responseLiveData.value = catFactApi.getCatFact()
+        }
+    }
 }
