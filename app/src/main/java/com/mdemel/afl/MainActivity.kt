c297fe0a07627c2e977cd722827b034e9190918a
MDeMel-Dev <mdemel.dev@gmail.com>
2023-10-12 21:37:44 +1100

    Implement Glide to render image in imageView

--- app/src/main/java/com/mdemel/afl/MainActivity.kt
+++ app/src/main/java/com/mdemel/afl/MainActivity.kt
@@ -2,9 +2,11 @@ package com.mdemel.afl
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.widget.AppCompatButton
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,16 +26,22 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.buttonGetDogImage)
     }
 
+    private val dogImage: ImageView by lazy {
+        findViewById(R.id.dogImage)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         viewModel.catFactResponseLiveData.observe(this) { response ->
-            textLabel.text = response?.fact ?: "Hello World\nWelcome to the world of\nCat Facts"
+            textLabel.text = response?.fact ?: "Hello World\nWelcome to the world of\nCat Facts and Dog Images."
         }
 
         viewModel.dogImageResponseLiveData.observe(this) { response ->
-            
+            Glide.with(this)
+                .load(response?.message)
+                .into(dogImage)
         }
 
         buttonGetCatFact.setOnClickListener {
