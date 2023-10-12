63718c972c4d8366822a2961b732de4c7d8ac36c
MDeMel-Dev <mdemel.dev@gmail.com>
2023-10-12 20:17:34 +1100

    import Moshi dependancy

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,9 +40,12 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Third party dependancies
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.4.0")
 }
