c7161114e49c9e253e98cf337c9989e70e9b925b
MDeMel-Dev <mdemel.dev@gmail.com>
2023-10-16 21:27:14 +1100

    get response from retrofit with the expected model

--- app/src/main/java/com/mdemel/afl/api/JokesApi.kt
+++ app/src/main/java/com/mdemel/afl/api/JokesApi.kt
@@ -1,9 +1,10 @@
 package com.mdemel.afl.api
 
+import com.mdemel.afl.model.JokesList
 import retrofit2.http.GET
 
 interface JokesApi {
 
     @GET("api/jokes")
-    suspend fun getJokes(): List<String>
+    suspend fun getJokes(): JokesList
 }
